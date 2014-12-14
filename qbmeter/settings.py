d055fa76717ef42d94f8defcd0f1a85fd716931f
orangain <orangain@gmail.com>
2014-12-14 13:30:29 +0900

    Create project

---
+++ qbmeter/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for qbmeter project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'qbmeter'
+
+SPIDER_MODULES = ['qbmeter.spiders']
+NEWSPIDER_MODULE = 'qbmeter.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'qbmeter (+http://www.yourdomain.com)'
