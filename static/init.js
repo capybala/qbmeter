5bc2a602758fb9401416a8511052b7d0ee8fc250
makincho <makiko9680@gmail.com>
2014-12-23 16:13:49 +0900

    デザインを入れました

---
+++ static/init.js
@@ -0,0 +1,23 @@
+//スティッキーヘッダー
+var $window = $(window), //ウィンドウを指定
+$content = $("#main"), //#content部分
+$chenge = $("#change"), //#change部分
+topContent = $content.offset().top; //#contentの位置を取得
+topContent = 50; //$("#header").height() - 243 * 0.4;
+
+var sticky = false;
+
+ $window.on("scroll", function () {
+      if ($window.scrollTop() > topContent) {　//scroll位置が#contentの上にある場合
+           if ( sticky === false ){
+                $chenge.slideDown();　//#change部分が上がる。
+                sticky = true;
+           }
+      } else {
+           if ( sticky === true ){　//scroll位置が下にある場合
+                $chenge.slideUp();//#change部分が降りてくる。
+                sticky = false;
+           }
+      }
+ });
+ $window.trigger("scroll");
